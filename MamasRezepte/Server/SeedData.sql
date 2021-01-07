55edda09b793d84e2c220c6ec9084ddb87d7ac9c
Lukas Thomas Nadenau <lukas.nadenau@rwth-aachen.de>
2021-01-07 20:17:53 +0100

    Sql-Datei zum Füllen der Db mit Startdaten

---
+++ MamasRezepte/Server/SeedData.sql
@@ -0,0 +1,18 @@
+﻿INSERT INTO
+	"Categories"("Name")
+VALUES
+	('Fleisch und Geflügel'),
+	('Fisch und Meeresfrüchte'),
+	('Suppen und Eintöpfe'),
+	('Salate und Snacks'),
+	('Dessert und Süßspeisen'),
+	('Kuchen und Gebäck');
+
+
+INSERT INTO
+	"DurationCategories"("Name")
+VALUES
+	('weniger als 30 min.'),
+	('30 - 60 min.'),
+	('mehr als 60 min.');
+
